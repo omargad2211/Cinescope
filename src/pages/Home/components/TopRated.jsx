a73b9561f73efeb2574c41657dd48b0884f31913
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-05 13:38:39 +0200

    feat: add top rated component

---
+++ src/pages/Home/components/TopRated.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import MovieCard from "../../../components/MovieCard";
+import {
+  useGetTopRatedQuery,
+  useGetTrendingQuery,
+} from "../../../redux/apiData/getDataSlice";
+
+const TopRated = () => {
+  const { data: topRated, isLoading, isError } = useGetTopRatedQuery();
+  console.log(topRated?.results);
+
+  return (
+    <div className="relative flex flex-col justify-start items-start gap-3 z-50 w-full container md:w-custom-md xl:w-custom-xl mx-auto pt-24">
+      <p className="font-semibold text-2xl text-neutral-400">
+        Top Rated Movies
+      </p>
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 w-full">
+        {topRated?.results.map((TopRated) => {
+          return (
+            <MovieCard
+              key={TopRated.id}
+              movie={TopRated}
+              path={`movie/${TopRated?.id}`}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default TopRated;
