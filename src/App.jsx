4f0abe431341917469f050983ecab765bbc24e7f
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-08 20:18:37 +0200

    feat: add ConditionalExplore route

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useParams } from "react-router-dom";
 import Home from "./pages/Home";
 import PublicLayout from "./Layouts/PublicLayout";
 import MovieDetails from "./pages/MovieDetails";
+import Explore from "./pages/Explore";
+import Favourite from "./pages/Favourite";
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route element={<PublicLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="movie/:movieID" element={<MovieDetails />} />
-        </Route>
-      </Routes>
-    </>
+    <Routes>
+      <Route element={<PublicLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path=":explore/:movieID" element={<MovieDetails />} />
+        <Route path=":explore" element={<ConditionalExplore />} />
+      </Route>
+    </Routes>
   );
+}
+
+function ConditionalExplore() {
+  const { explore } = useParams();
+
+  return explore === "favourite" ? <Favourite /> : <Explore />;
 }
 
 export default App;
