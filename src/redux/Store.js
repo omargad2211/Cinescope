8b74b9cad55ba962df3a23b3c05f00b5d145c04c
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-09 03:19:55 +0200

    feat: add wishList slice

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { apiSlice } from "./api/apiSlice";
 import { movieSlice } from "./MovieSlice/MovieSlice";
+import wishlistReducer from "./wishList/wishlistSlice";
 
 export const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     movies: movieSlice.reducer,
+    wishlist: wishlistReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
---
+++ src/redux/wishList/wishlistSlice.js
@@ -0,0 +1,75 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+
+// Function to load the Wishlist from LocalStorage
+const loadWishlistFromLocalStorage = () => {
+  const savedWishlist = localStorage.getItem("Wishlist");
+  return savedWishlist ? JSON.parse(savedWishlist) : [];
+};
+
+// Function to save the Wishlist to LocalStorage
+const saveWishlistToLocalStorage = (wishlistItems) => {
+  localStorage.setItem("Wishlist", JSON.stringify(wishlistItems));
+};
+
+const WishlistSlice = createSlice({
+  name: "Wishlist",
+  initialState: {
+    items: loadWishlistFromLocalStorage(),
+  },
+  reducers: {
+    addToWishlist: (state, action) => {
+      const {
+        id,
+        name,
+        description,
+        image,
+        rate,
+        date,
+        duration,
+        categories,
+        isSelected,
+      } = action.payload;
+      const existingItem = state.items.find(
+        (item) => item.id === id
+      );
+
+      if (!existingItem) {
+        // Add new item to Wishlist
+        state.items.push({
+          id,
+          name,
+          description,
+          image,
+          rate,
+          date,
+          duration,
+          categories,
+          isSelected,
+        });
+        saveWishlistToLocalStorage(state.items);
+        // toast.success('Film added to Favourites');
+      }
+    },
+    removeFromWishlist: (state, action) => {
+      const { id, subjectID } = action.payload;
+      state.items = state.items.filter(
+        (item) => !(item.id === id && item.subjectID === subjectID)
+      );
+      saveWishlistToLocalStorage(state.items);
+      // toast.error("Film removed from Favourites");
+    },
+    clearWishlist: (state) => {
+      state.items = [];
+      saveWishlistToLocalStorage(state.items);
+    },
+  },
+});
+
+export const { addToWishlist, removeFromWishlist, clearWishlist } =
+  WishlistSlice.actions;
+export default WishlistSlice.reducer;
+
+// Selector to calculate total items in the Wishlist
+export const selectTotalItems = (state) => state.Wishlist.items.length;
