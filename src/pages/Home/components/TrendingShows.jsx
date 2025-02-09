7eebcf049966370c032ee1597579194d7b02a9c6
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-09 12:23:08 +0200

    style: fix grid system

--- src/pages/Home/components/TrendingShows.jsx
+++ src/pages/Home/components/TrendingShows.jsx
@@ -11,7 +11,7 @@ const TrendingShows = () => {
   return (
     <div className="relative flex flex-col justify-start items-start gap-3 z-50 w-full container md:w-custom-md xl:w-custom-xl mx-auto pt-24">
       <p className="font-semibold text-2xl text-neutral-400">Trending Show</p>
-      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 w-full">
+      <div className="grid grid-cols-3 lg:grid-cols-5 gap-4 w-full">
         {bannerData?.map((trending) => {
           return (
             <MovieCard
