5b95fd017f8c2603323921af49a461cb176006a3
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-10 03:41:26 +0200

    feat: add close icon to seaech result

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { IoIosSearch, IoIosStar } from "react-icons/io";
 import { useGetSearchQuery } from "../redux/apiData/getDataSlice";
 import { Link } from "react-router-dom";
+import { CiCircleRemove } from "react-icons/ci";
 
 const Search = () => {
   const [pageNum, setPageNum] = useState(1);
@@ -28,47 +29,54 @@ const Search = () => {
       {/* Only render the results div if searchQuery has a value and searchData exists */}
       {searchQuery && searchData?.results?.length > 0 && (
         <div className="absolute top-[65px] z-[51] rounded-3xl right-0 w-full h-[440px] shadow-black/10 backdrop-blur-[5px] border border-white/60 overflow-y-auto scrollbar-hide searchglass">
-          <div className="absolute top-0 h-full w-full rounded-3xl"></div>
-          {searchData?.results?.map(
-            (item) =>
-              item.poster_path != null && (
-                <Link
-                  onClick={() => setSearchQuery("")}
-                  key={item.id}
-                  to={
-                    item.media_type === "tv"
-                      ? `/tv/${item.id}`
-                      : `/movie/${item.id}`
-                  }
-                >
-                  <div className="flex gap-x-2 px-3 rounded-b-3xl border-b border-[#ffffff1f] py-2 items-center relative z-[55]">
-                    <img
-                      src={`https://image.tmdb.org/t/p/w500/${item?.poster_path}`}
-                      width={50}
-                      height={50}
-                      alt={`${item?.original_title || item?.name}`}
-                      className="rounded-lg z-[55]"
-                    />
-                    <div className="flex flex-col justify-center items-start flex-wrap gap-x-2 z-[55]">
-                      <p className="text-white text-[12px]">
-                        {item?.original_title || item?.name}
-                      </p>
-                      <div className="flex items-center justify-center gap-x-1">
+          <button
+            onClick={() => setSearchQuery("")}
+            className="text-neutral-300 absolute right-2 top-2 text-2xl"
+          >
+            <CiCircleRemove />
+          </button>
+          <div className="absolute top-8 h-full w-full rounded-3xl">
+            {searchData?.results?.map(
+              (item) =>
+                item.poster_path != null && (
+                  <Link
+                    onClick={() => setSearchQuery("")}
+                    key={item.id}
+                    to={
+                      item.media_type === "tv"
+                        ? `/tv/${item.id}`
+                        : `/movie/${item.id}`
+                    }
+                  >
+                    <div className="flex gap-x-2 px-3 rounded-b-3xl border-b border-[#ffffff1f] py-2 items-center relative z-[55]">
+                      <img
+                        src={`https://image.tmdb.org/t/p/w500/${item?.poster_path}`}
+                        width={50}
+                        height={50}
+                        alt={`${item?.original_title || item?.name}`}
+                        className="rounded-lg z-[55]"
+                      />
+                      <div className="flex flex-col justify-center items-start flex-wrap gap-x-2 z-[55]">
                         <p className="text-white text-[12px]">
-                          <IoIosStar className="text-yellow-400 mb-1 inline" />
-                          {item?.vote_average?.toString().slice(0, 3)}
+                          {item?.original_title || item?.name}
                         </p>
-                        <p className="text-white text-[12px]">
-                          |{" "}
-                          {item?.release_date?.toString().slice(0, 4) ||
-                            item?.first_air_date?.toString().slice(0, 4)}
-                        </p>
+                        <div className="flex items-center justify-center gap-x-1">
+                          <p className="text-white text-[12px]">
+                            <IoIosStar className="text-yellow-400 mb-1 inline" />
+                            {item?.vote_average?.toString().slice(0, 3)}
+                          </p>
+                          <p className="text-white text-[12px]">
+                            |{" "}
+                            {item?.release_date?.toString().slice(0, 4) ||
+                              item?.first_air_date?.toString().slice(0, 4)}
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </Link>
-              )
-          )}
+                  </Link>
+                )
+            )}
+          </div>
         </div>
       )}
 
