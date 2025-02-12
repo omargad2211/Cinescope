b51582683e5bec8d72efea1f658e409746cbb5fb
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-12 21:40:44 +0200

    feat: add actor details layout

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Explore from "./pages/Explore";
 import Favorites from "./pages/Favourite";
 import Mood from "./pages/mood";
 import ScrollTop from "./Helpers/ScrollTop";
+import ActorDetails from "./pages/Actor";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path=":explore/:movieID" element={<MovieDetails />} />
           <Route path=":explore" element={<ConditionalExplore />} />
+          <Route path="actor/:actorID" element={<ActorDetails />} />
         </Route>
       </Routes>
     </>
