3900245b2d10cdbc6cfdf242c87e76dfd43bcf58
omar gad <152779410+omargad2211@users.noreply.github.com>
2025-02-12 22:00:00 +0200

    fix: fix null actor movies

--- src/pages/Actor/components/ActorWork.jsx
+++ src/pages/Actor/components/ActorWork.jsx
@@ -12,12 +12,16 @@ const ActorWork = () => {
 
   const { data: actorMovies } = useGetActorMoviesQuery(actorID);
 
-//   console.log(actorMovies);
+  //   console.log(actorMovies);
+  const displayedMovies = actorMovies?.cast?.filter(
+    (movie) => movie.backdrop_path !== null
+  );
+  //   console.log(displayedMovies);
   return (
     <div className="relative flex flex-col justify-start items-start gap-3 z-50 w-full container md:w-custom-md xl:w-custom-xl mx-auto py-24">
       <p className="font-semibold text-2xl text-neutral-400">Actor Works</p>
       <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 w-full">
-        {actorMovies?.cast?.map((movie) => {
+        {displayedMovies?.map((movie) => {
           return (
             <MovieCard
               key={movie.id}
